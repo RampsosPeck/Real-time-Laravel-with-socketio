5d50177efc3e02e49e583b028e8438ccf8660419
RampsosPeck <peralta.jorge.uatf@gmail.com>
2019-08-23 01:46:42 -0400

    Complet lesson 1 COMPLET PERFECT

---
+++ index.js
@@ -0,0 +1,32 @@
+
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+
+
+server.listen(3000);
+
+app.get('/', function(request, res){
+	//res.send('Hello World');
+	res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', function(socket){
+	//console.log('Aconnection was made.');
+	socket.on('chat.message', function(message){
+		//console.log('New Message: '+message);
+
+		io.emit('chat.message', message);
+	});
+
+	socket.on('disconnect', function(){
+		//console.log('New Message: '+message);
+
+		io.emit('chat.message', 'El usuario se desconecto.');
+	});
+
+
+});
+
